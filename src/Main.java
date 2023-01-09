74c0ea5a220eb51a379360c0348e85544dd16fb7
DmitryMorozov62 <62morozov.dima.90@gmail.com>
2023-01-09 18:57:11 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/* Решение седьмой домашний работы (7 из 16)
+оператор цикла for
+09.01.2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ПЕрвый коммит");
+    }
+
+}
